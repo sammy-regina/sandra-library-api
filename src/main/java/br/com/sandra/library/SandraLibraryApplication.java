79eed89a2547fbe4793e90f80df0c9953a9a28a3
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2026-02-15 19:39:05 -0300

    chore: initial project structure for Sandra Library

---
+++ src/main/java/br/com/sandra/library/SandraLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.sandra.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandraLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandraLibraryApplication.class, args);
+	}
+
+}
