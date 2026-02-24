f29b55149c1d70f386d1385c81e71314dc360063
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2026-02-24 20:49:46 -0300

    create: criado método delete

--- src/main/java/br/com/sandra/library/api/controller/BookController.java
+++ src/main/java/br/com/sandra/library/api/controller/BookController.java
@@ -3,13 +3,17 @@ package br.com.sandra.library.api.controller;
 
 import br.com.sandra.library.domain.model.BookEntity;
 import br.com.sandra.library.domain.repository.BookRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/books")
@@ -29,6 +33,12 @@ public class BookController {
     @PostMapping
     public BookEntity create(@RequestBody BookEntity book) {
         return repository.save(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
