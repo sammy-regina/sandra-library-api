2a00f01711648837d8107edd3db39bc6cb7dd8a0
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2026-02-24 21:25:57 -0300

    create: criado método put

--- src/main/java/br/com/sandra/library/api/controller/BookController.java
+++ src/main/java/br/com/sandra/library/api/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,21 @@ public class BookController {
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         repository.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookEntity> update(@PathVariable UUID id,
+                                             @RequestBody BookEntity bookDetails) {
+        return repository.findById(id)
+                .map(book -> {
+                    book.setTitle(bookDetails.getTitle());
+                    book.setAuthor(bookDetails.getAuthor());
+                    book.setPages(bookDetails.getPages());
+
+                    BookEntity updatedBook = repository.save(book);
+                    return ResponseEntity.ok(updatedBook);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
