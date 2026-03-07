88091db1b9d126b06fd335d14926490a190d2d85
sammy-regina <145820828+sammy-regina@users.noreply.github.com>
2026-03-07 18:41:36 -0300

    feature:criados metodos PUT e DELETE para categories

--- src/main/java/br/com/sandra/library/api/controller/CategoryController.java
+++ src/main/java/br/com/sandra/library/api/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package br.com.sandra.library.api.controller;
 
+import br.com.sandra.library.domain.model.BookEntity;
 import br.com.sandra.library.domain.model.CategoryEntity;
 import br.com.sandra.library.domain.repository.CategoryRepository;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/categories")
@@ -26,5 +28,23 @@ public class CategoryController {
     @GetMapping
     public List<CategoryEntity> listAll() {
         return repository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryEntity> update(@PathVariable UUID id,
+                                                 @RequestBody CategoryEntity categoryDetails) {
+        return repository.findById(id)
+                .map(category -> {
+                    category.setName(categoryDetails.getName());
+                    CategoryEntity updatedCategory = repository.save(category);
+                    return ResponseEntity.ok(updatedCategory);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
